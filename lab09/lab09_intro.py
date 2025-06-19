008e9ac9bc216bc860d3a1a87d927d13d4f1abd7
jgomezz <jgomezz@gmail.com>
2025-06-18 19:57:16 -0500

    update

---
+++ lab09/lab09_intro.py
@@ -0,0 +1,13 @@
+
+# Ejercicio 03
+
+import numpy as np
+
+Trafico = np.array([[  0,   50,   30,   20 ],
+                    [ 40,    0,   60,   25 ],
+                    [ 35,   45,    0,   40 ],
+                    [ 15,   30,   50,    0 ]
+                    ])
+
+print(Trafico)
+
