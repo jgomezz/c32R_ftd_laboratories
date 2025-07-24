d7c926915189aaae66cbf7369b5ac69331843068
jgomezz <jgomezz@gmail.com>
2025-07-23 19:06:21 -0500

    begin 14 lab.

---
+++ lab14/lab14_01.py
@@ -0,0 +1,11 @@
+
+
+import numpy as np
+import statistics
+
+ventas = [150, 200, 120, 180, 250, 100, 170]
+
+# Media
+media_ventas = statistics.mean(ventas)
+
+print(f"Media de ventas: {media_ventas:.2f}")
