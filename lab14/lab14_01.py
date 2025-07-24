831b06e43fc157cdec8531d9eca89442cb4c782a
jgomezz <jgomezz@gmail.com>
2025-07-23 19:16:24 -0500

    begin 14 lab.

--- lab14/lab14_01.py
+++ lab14/lab14_01.py
@@ -9,3 +9,7 @@ ventas = [150, 200, 120, 180, 250, 100, 170]
 media_ventas = statistics.mean(ventas)
 
 print(f"Media de ventas: {media_ventas:.2f}")
+
+sd = statistics.stdev(ventas)
+print(f"Desviacion Estandar: {sd:.2f}")
+
